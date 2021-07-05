6f0a62214fa2f122745d921bbbb647235c43df43
Timothy Gordon <tgordon523@gmail.com>
2021-07-05 11:59:59 -0500

    wk16 details

--- Week16/week_16.py
+++ Week16/week_16.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 df = pd.read_excel(Path.cwd()/'test.xlsx', engine="openpyxl")
 
+print(df.line.tail())
 ### regex pattern to extract the website details from unstructured text
 pat = r'(?P<Username>[a-z0-9_\.-]+)@(?P<website>[\da-z\.-]+)\.(?P<domain>[a-z\.]{2,6})'
 ### extract from the text and separate
 df_extract = df.line.str.extract(pat)
 
 print(df_extract.info())
+### showcase results
 print(df_extract)

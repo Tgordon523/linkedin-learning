acaf6f2e2ebd34bc59668c04b06e503962ddcfb4
Tgordon523 <tgordon523@gmail.com>
2021-07-19 17:11:32 -0500

    ignore vscode folders

--- Week18/week_18.py
+++ Week18/week_18.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import numpy as np
-from pathlib import Path
 
 
-### regex pattern to extract the currency and amount from unstructured text
+### Create dataset
 df = pd.DataFrame(
     np.random.normal(7.5, 2, size=(100, 6)),
     columns=["Math_1", "Math_2", "Math_3", "EngLit_1", "EngLit_2", "EngLit_3"],
 )
 
 
-print(df)
-
-print(df.loc[:, "Math_3":"EngLit_2"].head())
+print("Simple Filter everything between \n", df.loc[:, "Math_3":"EngLit_2"].head())
 
 math_mask = df.columns[df.columns.str.startswith("Math")]
 print(df[math_mask])

0c0ebe95241b38f29561c4718d9e52b4b28cbc3d
Tgordon523 <tgordon523@gmail.com>
2021-08-15 16:53:06 -0500

    wk21 updates

--- Week21/week_21.py
+++ Week21/week_21.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import size
 import pandas as pd
 import numpy as np
 
@@ -7,6 +8,9 @@ df = pd.DataFrame(
     np.random.normal(7.5, 2, size=(100, 6)),
     columns=["Math 1", "Math 2", "Math 3", "EngLit 1", "EngLit 2", "EngLit 3"],
 )
+
+teachers = np.random.choice(["Karen", "Dwight", "Doug"], size=100)
+df["Teachers"] = teachers
 
 print(f"Original dataframe names {df.columns.values} \n")
 
@@ -19,3 +23,11 @@ def clean_names(df: pd.DataFrame) -> pd.DataFrame:
 
 
 print(clean_names(df))
+df1 = clean_names(df)
+df1["id"] = df1.index
+# print(df1)
+
+df2 = pd.wide_to_long(df1, stubnames=["math", "englit"], sep="_", i=["id"], j="year")
+print(df2)
+
+# print(df1.unstack(-1).reset_index())
